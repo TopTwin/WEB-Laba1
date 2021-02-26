3806f0a6e6e2a6aeeb50cbc01feaeb7eac4bf1ac
Evgeny Stepanov <kingsis22333@gmail.com>
2021-02-26 16:11:30 +0300

    na 4etvero4ky

--- scrypt.js
+++ scrypt.js
@@ -1,8 +1,10 @@
+let out = document.getElementById("output");
 window.addEventListener('load', () => {
     const input = document.getElementById("category");
     input.addEventListener('keypress', (e) => {
         if (e.key === 'Enter') {
             //console.log(input.value);
+            out.innerText = "";
             sendRequest(input.value);
         }
     })
@@ -13,8 +15,14 @@ function sendRequest(input){
             .then(res => res.json())
             .then(json => {
                 json.forEach(element => {
-                    if (input === element.category)
-                    console.log(element)    
+                    if (input == element.category){
+                    //console.log(element)
+                    out.innerText += "Title: " + element.title + "\n";
+                    out.innerText += "Category: " + element.category + "\n";
+                    out.innerText += "Description: " + element.description + "\n";
+                    out.innerText += "Price: " + element.price + "\n";
+                    out.innerText += "\n\n\n";    
+                    }
                 });
             })
 }
@@ -24,9 +32,52 @@ window.addEventListener('load', () => {
     but.addEventListener('click', () => {
         var categoryes = document.querySelectorAll(".radio input");
         categoryes.forEach(cat => {
-            if (cat.checked === true)
+            if (cat.checked === true) {
+                out.innerText = "";
+                choicePrice(cat.value);
+            }
             //console.log(cat.value);
-            sendRequest(cat.value);
         })
     })
 })
+
+function choicePrice(inputPrice){
+    fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(json => {
+                json.forEach(element => {
+                    let low = 0;
+                    if(inputPrice == 25){
+                        low = 0;
+                    }
+                    if(inputPrice == 50){
+                        low = 25;
+                    }
+                    if(inputPrice == 100){
+                        low = 50;
+                    }
+                    if(inputPrice == 500){
+                        low = 100;
+                    }
+                    if(inputPrice == 1000){
+                        low = 500;
+                    }
+                    if (inputPrice >= element.price && element.price >= low){
+                    console.log(element)
+                    out.innerText += "Title: " + element.title + "\n";
+                    out.innerText += "Category: " + element.category + "\n";
+                    out.innerText += "Description: " + element.description + "\n";
+                    out.innerText += "Price: " + element.price + "\n";
+                    out.innerText += "\n\n\n";
+                }
+                });
+            })
+}
+
+window.addEventListener('load', () => {
+    const rad = document.getElementById()
+})
+
+//fetch('https://fakestoreapi.com/products')
+//            .then(res=>res.json())
+//            .then(json=>console.log(json))

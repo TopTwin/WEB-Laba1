c2041b4aa4d63fccc02739daaa7928f1c1260c39
Evgeny Stepanov <kingsis22333@gmail.com>
2021-03-01 12:54:21 +0300

    hh

--- scrypt.js
+++ scrypt.js
@@ -70,7 +70,3 @@ function choicePrice(inputPrice){
                 });
             })
 }
-
-//fetch('https://fakestoreapi.com/products/1')
-//           .then(res=>res.json())
-//           .then(json=>console.log(json))
